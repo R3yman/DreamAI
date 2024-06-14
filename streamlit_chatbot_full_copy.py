00a7165465eb60ecc6d6eaafb8ba57a8aade31be
Ramon <ramonzacharias44@googlemail.com>
2024-06-14 14:00:33 +0200

    Remove langsmith tracing

--- streamlit_chatbot_full_copy.py
+++ streamlit_chatbot_full_copy.py
@@ -15,7 +15,7 @@ import os
 from dotenv import load_dotenv
 
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
-os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_TRACING_V2"] = "false"
 os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
 os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
 
